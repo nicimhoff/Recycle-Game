591c00c4be6e8f9dbd2cfbfd488ee434f285826b
Bulat Bikmullin <bulatmegavolt@gmail.com>
2019-04-22 23:16:38 -0400

    changes

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,15 +1,3 @@
 import App from './App';
 export default App;
-import Amplify from "aws-amplify";
-import config from "./config";
 
-Amplify.configure({ Auth: {
-        mandatorySignIn: true,
-        region: config.cognito.REGION,
-        userPoolId: config.cognito.USER_POOL_ID, identityPoolId: config.cognito.IDENTITY_POOL_ID, userPoolWebClientId: config.cognito.APP_CLIENT_ID
-    }, Storage: {
-        region: config.s3.REGION,
-        bucket: config.s3.BUCKET,
-        identityPoolId: config.cognito.IDENTITY_POOL_ID
-    }
-});
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import configureStore from './configureStore';
 import * as serviceWorker from './serviceWorker';
 import App from './app';
+import Amplify from "aws-amplify";
+import config from "./config";
+
 
 const store = configureStore();
 const jsx = (
@@ -11,7 +14,16 @@ const jsx = (
     <App />
   </Provider>
 );
-
+Amplify.configure({ Auth: {
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID, identityPoolId: config.cognito.IDENTITY_POOL_ID, userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }, Storage: {
+        region: config.s3.REGION,
+        bucket: config.s3.BUCKET,
+        identityPoolId: config.cognito.IDENTITY_POOL_ID
+    }
+});
 ReactDOM.render(jsx, document.getElementById('root'));
 
 serviceWorker.unregister();
